a45141922fba02e733b01ef8943f73099cbb1d2f
Dat Pham <phamdat@flip2.engr.oregonstate.edu>
2018-07-22 15:29:58 -0700

    admin page upload

--- main.js
+++ main.js
@@ -17,6 +17,38 @@ app.set('port', process.argv[2]);
 app.set('mysql', mysql);
 app.use('/', express.static('public'));
 
+app.use('/admin', function (req, res, next){
+
+	displayAwardData(res);
+});
+
+app.use('/admin_award', function (req, res, next) {
+	displayAwardData(res);
+	
+});
+
+app.use('/admin_insert_aw', function (req, res, next) {
+	insertAward(req, res, next);
+	
+});
+
+app.use('/admin_delete_aw', function (req, res, next) {
+	deleteAward(req, res, next);
+
+});
+
+app.use('/admin_employee', function (req, res, next) {
+
+	res.render('admin_employee');
+});
+
+
+app.use('/admin_account', function (req, res, next) {
+
+	res.render('admin_account');
+});
+
+
 app.use(function(req,res){
   res.status(404);
   res.render('404');
@@ -31,3 +63,63 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
+
+/*********************************************************************************************
+ * Award Data
+ * *******************************************************************************************/
+
+var displayAwardData = function (res) {
+
+	var context = {};
+	var row_data = {};
+	row_data.award = [];
+
+	mysql.pool.query("SELECT * FROM bsg_award", function (err, rows, fields) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		for (row in rows) {
+			simple_award = {};
+			simple_award.id			 = rows[row].id;
+			simple_award.name		 = rows[row].name;
+			simple_award.description = rows[row].description;
+
+			row_data.award.push(simple_award);
+		}
+
+		context.results = JSON.stringify(rows);
+		context.data = row_data;
+		res.render('admin_award',context);
+	});
+
+};
+
+var insertAward = function (req, res,next) {
+	//console.log("name: %s", req.query.name);
+	//console.log("description: %s", req.query.description);
+	mysql.pool.query("INSERT INTO bsg_award(`name`,`description`) VALUES(?,?)",
+						[req.query.name, req.query.description], function (err, result) {
+
+		if (err) {
+			next(err);
+			return;
+		}
+		displayAwardData(res);
+	});
+};
+
+var deleteAward = function (req, res, next) {
+	//console.log("name: %s", req.query.name);
+	//console.log("description: %s", req.query.description);
+	mysql.pool.query("DELETE FROM bsg_award WHERE id=?", [req.query.id], function (err, result) {
+		if (err) {
+			next(err);
+			return;
+		}
+
+		//context.results = "Deleted " + result.changedRows + " rows.";
+		displayAwardData(res);
+	});
+};
---
+++ public/admin-script.js
@@ -0,0 +1,29 @@
+// Award
+function delete_row_award(current_row) {
+	var table = document.getElementById('award_table');
+	var req = new XMLHttpRequest();
+	var url = 'http://flip2.engr.oregonstate.edu:4247/admin_delete_aw?id=' + current_row.parentNode.parentNode.cells[0].textContent;
+
+	req.open('GET', url, true);
+	req.addEventListener('load', function () {
+		if (req.status >= 200 && req.status < 400)
+			console.log("Delete successful");
+		else
+			console.log("Error in network request: " + req.statusText);
+	});
+
+	req.send(null);
+	event.preventDefault();
+
+	//delete row in html
+	var row_count = table.rows.length;
+	for (var i = 0; i < row_count; i++) {
+		var row = table.rows[i];
+
+		if (row == current_row.parentNode.parentNode) {
+			table.deleteRow(i);
+			row_count--;
+			i--;
+		}
+	}
+}
