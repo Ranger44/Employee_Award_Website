d28844b43ea91b250a14e1e715b31c12fa6ad319
David Chen <chend7@flip2.engr.oregonstate.edu>
2018-07-22 00:42:27 -0700

    nodemailer and login

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_straleyh',
-  password        : '4961',
-  database        : 'cs340_straleyh'
+  user            : 'cs467-group1',
+  password        : 'E3NTBHHRrK8gxUbA',
+  database        : 'cs467-group1'
 });
 module.exports.pool = pool;
